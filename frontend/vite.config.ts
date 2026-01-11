76456771c0e29c138fea1dc133b4fa77c2b66e8c
BuildTools <unconfigured@null.spigotmc.org>
2026-01-11 20:54:28 +0100

    Opravena předání url adresy API

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,28 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv, type ConfigEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 import path from "path"
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://jedlickaf.cz:3000',
-        changeOrigin: true,
+export default defineConfig(({ mode }: ConfigEnv) => {
+  // Načtení env proměnných (process.cwd() je root projektu)
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-  },
+    server: {
+      proxy: {
+        '/api': {
+          // Použije VITE_API_BASE_URL, pokud existuje, jinak localhost
+          target: env.VITE_API_BASE_URL || 'http://localhost:3000',
+          changeOrigin: true,
+        },
+      },
+    },
+  }
 })
